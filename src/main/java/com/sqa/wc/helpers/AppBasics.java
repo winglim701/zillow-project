43b9f33372829c9a0681d0cc9d954fc6b5a677d2
William WL Chan <winglim@hotmail.com>
2017-03-08 18:40:17 -0800

    update AppBasics

--- src/main/java/com/sqa/wc/helpers/AppBasics.java
+++ src/main/java/com/sqa/wc/helpers/AppBasics.java
@@ -36,6 +36,43 @@ public class AppBasics {
 	}
 
 	/**
+	 * @param question
+	 * @return
+	 */
+	public static boolean requestBoolean(String question) {
+		String input;
+		boolean count = false;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			count = Boolean.parseBoolean(input.trim());
+		} catch (NumberFormatException e) {
+			System.out.println(
+					"You have not entered a correct boolean value (" + input + ")\n Using false for set value");
+		}
+		return count;
+	}
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	public static char requestChar(String question) {
+		/**
+		 * @param question
+		 * @return
+		 */
+		String input;
+		char character;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		character = input.charAt(0);
+		return character;
+	}
+
+	/**
 	 * @param string
 	 * @return
 	 */
@@ -50,12 +87,103 @@ public class AppBasics {
 		System.out.println(question + " ");
 		input = scanner.nextLine();
 		try {
+			count = Double.parseDouble(input.trim());
+		} catch (NumberFormatException e) {
+			System.out.println("You have not entered a correct double value (" + input + ")\n Using 0 for set value");
+		}
+		return count;
+	}
+
+	public static float requestFloat(String question) {
+		/**
+		 * @param question
+		 * @return
+		 */
+		String input;
+		float count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			count = Float.parseFloat(input.trim());
+		} catch (NumberFormatException e) {
+			System.out
+					.println("You have not entered a correct numerical value (" + input + ")\n Using 0 for set value");
+		}
+		return count;
+	}
+
+	/**
+	 * @param string
+	 * @return
+	 */
+	public static int requestInt(String question) {
+		/**
+		 * @param question
+		 * @return
+		 */
+		String input;
+		int count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
 			count = Integer.parseInt(input.trim());
 		} catch (NumberFormatException e) {
 			System.out
 					.println("You have not entered a correct numerical value (" + input + ")\n Using 0 for set value");
 		}
 		return count;
+	}
+
+	public static long requestLong(String question) {
+		/**
+		 * @param question
+		 * @return
+		 */
+		String input;
+		long count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			count = Long.parseLong(input.trim());
+		} catch (NumberFormatException e) {
+			System.out
+					.println("You have not entered a correct numerical value (" + input + ")\n Using 0 for set value");
+		}
+		return count;
+	}
+
+	public static short requestShort(String question) {
+		/**
+		 * @param question
+		 * @return
+		 */
+		String input;
+		short count = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		try {
+			count = Short.parseShort(input.trim());
+		} catch (NumberFormatException e) {
+			System.out
+					.println("You have not entered a correct numerical value (" + input + ")\n Using 0 for set value");
+		}
+		return count;
+	}
+
+	/**
+	 * @return
+	 *
+	 */
+	public static String requestString(String question) {
+		String input;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println(question + " ");
+		input = scanner.nextLine();
+		return input;
 	}
 
 	/**
